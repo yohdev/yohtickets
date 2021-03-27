af091924f8e6d8b96b43c9ff077d05209a1a72d1
vkaukeano <von.kaukeano@temple.edu>
2021-03-27 19:08:21 -0400

    Update

--- server/keys.js
+++ server/keys.js
@@ -1,12 +1,9 @@
-/*
-module.exports = {
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: process.env.PGPORT,
-};
 
-*/
+export const redisHost = process.env.REDIS_HOST;
+export const redisPort = process.env.REDIS_PORT;
+export const pgUser = process.env.PGUSER;
+export const pgHost = process.env.PGHOST;
+export const pgDatabase = process.env.PGDATABASE;
+export const pgPassword = process.env.PGPASSWORD;
+export const pgPort = process.env.PGPORT;
+
